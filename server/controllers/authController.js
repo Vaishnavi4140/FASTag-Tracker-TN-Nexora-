919ab35f2bea922bf10c1958fffe4d744d7149e4
singhvaishu005@gmail.com <vaishnavi.@Vaishnavis-MacBook-Air.local>
2026-05-06 22:05:10 +0530

    data stored in cluster

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,25 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 const registerUser = async (req, res) => {
-  res.json({ message: "Register working" });
+  try {
+    const { name, email, password } = req.body;
+
+    const user = new User({
+      name,
+      email,
+      password
+    });
+
+    await user.save(); // 
+
+    res.status(201).json({
+      message: "User registered",
+      user
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 const loginUser = async (req, res) => {
--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -28,4 +28,4 @@ const userSchema = new mongoose.Schema({
 { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
-export default User;
+module.exports = User;
